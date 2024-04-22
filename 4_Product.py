593dca86c6a0e27d4578ba3a0efe1f3a317cd859
BabashkinaOlesya <133039573+BabashkinaOlesya@users.noreply.github.com>
2024-04-22 23:11:00 +0700

    Update 4_Product.py

--- 4_Product.py
+++ 4_Product.py
@@ -6,30 +6,19 @@ import xml.dom.minidom as minidom
 # Класс Клиент (имя, адрес, номер телефона)
 @dataclass
 class Client:
-    client_id: int
     full_name: str
     address: str
     phone: str
 
-# Класс Администратор (имя, номер телефона, филиал в котором работает админ, время работы)
-@dataclass
-class Administrator:
-    administrator_id: int
-    full_name: str
-    phone: str
-    branch_id: int
-    time_job: str
 
 # Класс Филиал (адрес)
 @dataclass(frozen=True)
 class Branch:
-    branch_id: int
     address: str
 
 # Класс Товар (наименование, цена, количество в наличии)
 @dataclass
 class Product:
-    product_id: int
     name: str
     price: float
     count: int
@@ -37,17 +26,16 @@ class Product:
 # Класс Покупка (дата покупки, количество купленного товара, id клиента-покупателя, id филиала-места выдачи)
 @dataclass(frozen=True)
 class Purchase:
-    purchase_id: int
     date: str
     quantity: int
     client_id: int
     product_id: int
     branch_id: int
 
+
 # Класс Поставщик (наименование)
 @dataclass(frozen=True)
 class Provider:
-    provider_id: int
     name: str
 
 #Создание файла
@@ -60,7 +48,7 @@ if not os.path.exists(file_path):
 
     clients = ET.SubElement(root, 'clients')
     administrators = ET.SubElement(root, 'administrators')
-    branches = ET.SubElement(root, 'branches')
+    branchs = ET.SubElement(root, 'branchs')
     products = ET.SubElement(root, 'products')
     purchases = ET.SubElement(root, 'purchases')
     providers = ET.SubElement(root, 'providers')
@@ -87,42 +75,79 @@ root = tree.getroot()
 
 class PurchaseServise:
     @staticmethod
-    def check_purchase(quantity, product_id):
-        chosen_product = next((product for product in products if product.product_id == product_id), None)
+    def check_purchase(purchase, product, product_count_available, branch):
 
-        if not chosen_product:
-            print("Выбранный товар не существует.")
+        if product is None:
+            print('Выбранный товар не существует')
             return False
 
-        if quantity > chosen_product.count:
-            print("Недостаточное количество товара.")
+        if branch is None:
+            print('Выбранный филиал недоступен')
             return False
 
+        if purchase.quantity > int(product_count_available):
+            print("Товара недостаточно на складе")
+            return False
 
-        chosen_product.count -= quantity
-        print(f"Покупка товара {chosen_product.name} успешно оформлена. Остаток: {chosen_product.count}")
         return True
 
 
 class SupplyServise:
     @staticmethod
-    def check_supply(product_id, counts_provide, provider_id):
-        chosen_provider = next((provider for provider in providers if provider.provider_id == provider_id), None)
-
-        if not chosen_provider:
-            print("Выбранный поставщик не существует.")
+    def check_supply(product, provider):
+        if product is None:
+            print('Выбранный товар не существует')
             return False
 
-        chosen_product = next((product for product in products if product.product_id == product_id), None)
-
-        if not chosen_product:
-            print("Выбранный товар не найден.")
+        if provider is None:
+            print('Выбранный поставщик не существует')
             return False
-
-        chosen_product.count += counts_provide
-        print(
-            f"Поставка товара {chosen_product.name} от поставщика {chosen_provider.name}. Обновленное количество: {chosen_product.count}")
         return True
+
+
+class ServiceLayer:
+    @staticmethod
+    def add_object(objectt):
+        if isinstance(objectt, Client):
+            id=repository.get_id_new('client')
+            repository.add_entity(objectt, 'client', id)
+
+        if isinstance(objectt, Branch):
+            id=repository.get_id_new('branch')
+            repository.add_entity(objectt, 'branch', id)
+
+        if isinstance(objectt, Product):
+            id=repository.get_id_new('product')
+            repository.add_entity(objectt, 'product', id)
+
+        if isinstance(objectt, Provider):
+            id=repository.get_id_new('provider')
+            repository.add_entity(objectt, 'provider', id)
+
+        if isinstance(objectt, Purchase):
+            id=repository.get_id_new('purchase')
+            product = repository.get_entiti_by_id('product', objectt.product_id)
+            product_count_available = repository.get_product_count(objectt.product_id)
+            branch = repository.get_entiti_by_id('branch', objectt.branch_id)
+            if PurchaseServise.check_purchase(objectt, product, product_count_available, branch):
+                repository.add_entity(objectt, 'purchase', id)
+                repository.update_product_count(objectt.product_id, objectt.quantity, '-')
+
+
+    @staticmethod
+    def remove_object(object_name, id):
+        repository.remove_entity(object_name, id)
+
+    @staticmethod
+    def get_all_object(object_name):
+        print(repository.get_all_entities(object_name))
+
+    @staticmethod
+    def order_a_product(product_id, provider_id, count):
+        product = repository.get_entiti_by_id('product', product_id)
+        provider = repository.get_entiti_by_id('provider', provider_id)
+        if SupplyServise.check_supply(product, provider):
+            repository.update_product_count(product_id, count, '+')
 
 
 class XMLRepository:
@@ -131,167 +156,89 @@ class XMLRepository:
         self.data = ET.parse(file_path)
         self.root = self.data.getroot()
 
-    def add_administrator(self, administrator):
+    def get_id_new(self, entity_name):
+        # Нахождение максимального id для элементов
+        max_id = 0
+        for entiti in self.root.findall(f'.//{entity_name}'):
+            entiti_id = int(entiti.attrib.get('id', 0))
+            max_id = max(max_id, entiti_id)
 
-        administrators = self.root.find('administrators')
+        print(f"Максимальный id для элементов: {max_id}")
+        return max_id+1
 
-        new_administrator = ET.SubElement(administrators, 'administrator')
-        ET.SubElement(new_administrator, 'administrator_id').text = str(administrator.administrator_id)
-        ET.SubElement(new_administrator, 'full_name').text = administrator.full_name
-        ET.SubElement(new_administrator, 'phone').text = administrator.phone
-        ET.SubElement(new_administrator, 'branch_id').text = str(administrator.branch_id)
-        ET.SubElement(new_administrator, 'time_job').text = administrator.time_job
+    def add_entity(self, entity, entity_type, id):
+        parent_element = self.root.find(entity_type + "s")
 
-        self.indent(self.data.getroot())
-        self.data.write(self.file_path)
+        new_entity = ET.SubElement(parent_element, entity_type)
+        new_entity.set('id', str(id))
 
-        print("Администратор успешно добавлен.")
+        for prop, value in vars(entity).items():
+            if not prop.startswith("_"):
+                ET.SubElement(new_entity, prop).text = str(value)
 
-    def get_administrators(self):
-        administrators = []
-        for administrator_elem in self.root.findall('administrators/administrator'):
-            administrator = Administrator(
-                int(administrator_elem.find('administrator_id').text),
-                administrator_elem.find('full_name').text,
-                administrator_elem.find('phone').text,
-                int(administrator_elem.find('branch_id').text),
-                administrator_elem.find('time_job').text
-            )
-            administrators.append(administrator)
-        return administrators
+        self.indent(self.data.getroot())  # Форматирование XML
+        self.data.write(self.file_path)  # Сохранение изменений в файл
 
-    def add_client(self, client):
-        clients = self.root.find('clients')
+        print(f"{entity_type.capitalize()} успешно добавлен.")
 
-        new_client = ET.SubElement(clients, 'client')
-        ET.SubElement(new_client, 'client_id').text = str(client.client_id)
-        ET.SubElement(new_client, 'full_name').text = client.full_name
-        ET.SubElement(new_client, 'address').text = client.address
-        ET.SubElement(new_client, 'phone').text = client.phone
+    def remove_entity(self, entity_type, id):
+        parent_element = self.root.find(entity_type + "s")
 
-        self.indent(self.data.getroot())
-        self.data.write(self.file_path)
+        for element in parent_element.findall(entity_type):
+            if element.get("id") == str(id):
+                parent_element.remove(element)
+                self.indent(self.data.getroot())
+                self.data.write(self.file_path)
+                print(f"{entity_type.capitalize()} с id {id} успешно удален.")
+                return
 
-        print("Клиент успешно добавлен.")
+        print(f"{entity_type.capitalize()} с id {id} не найден.")
 
-    def get_clients(self):
-        clients = []
-        for client_elem in self.root.findall('clients/client'):
-            client = Client(
-                int(client_elem.find('client_id').text),
-                client_elem.find('full_name').text,
-                client_elem.find('address').text,
-                client_elem.find('phone').text
-            )
-            clients.append(client)
-        return clients
+    def get_entiti_by_id(self, entiti_name, id):
+        for entiti in root.findall(f'.//{entiti_name}'):
+            if entiti.attrib.get('id') == str(id):
+                return entiti
 
-    def add_product(self, product):
-        products = self.root.find('products')
+    def get_product_count(self, product_id):
+        product_element = self.root.find("products")
+        for product in product_element.findall("product"):
+            if product.get('id') == str(product_id):
+                count_element = product.find('count')
+                return count_element.text
 
-        new_product = ET.SubElement(products, 'product')
-        ET.SubElement(new_product, 'product_id').text = str(product.product_id)
-        ET.SubElement(new_product, 'name').text = product.name
-        ET.SubElement(new_product, 'price').text = str(product.price)
-        ET.SubElement(new_product, 'count').text = str(product.count)
 
-        self.indent(self.data.getroot())
-        self.data.write(self.file_path)
 
-        print("Продукт успешно добавлен.")
+    def get_all_entities(self, entity_type):
+        parent_element = self.root.find(entity_type + "s")
+        entities = []
 
-    def get_products(self):
-        products = []
-        for product_elem in self.root.findall('products/product'):
-            product = Product(
-                int(product_elem.find('product_id').text),
-                product_elem.find('name').text,
-                float(product_elem.find('price').text),
-                int(product_elem.find('count').text)
-            )
-            products.append(product)
-        return products
+        for element in parent_element.findall(entity_type):
+            entity_data = {}
+            entity_data["id"] = element.get("id")
+            for child in element:
+                entity_data[child.tag] = child.text
+            entities.append(entity_data)
 
-    def add_product_count(self, product_id, counts_provider, provider_id):
-        SupplyServise.check_supply(product_id=product_id, counts_provide=counts_provider, provider_id=provider_id)
-        print("Поставка успешно добавлена.")
+        return entities
 
-    def add_branch(self, branch):
-        branches = self.root.find('branches')
+    def update_product_count(self, product_id, count_diff, operation):
+        product_element = self.root.find("products")
+        for product in product_element.findall("product"):
+            if product.get('id') == str(product_id):
+                if operation == '+':
+                    count_element = product.find('count')
+                    new_count = int(count_element.text) + count_diff
+                    count_element.text = str(new_count)
+                elif operation == '-':
+                    count_element = product.find('count')
+                    new_count = int(count_element.text) - count_diff
+                    count_element.text = str(new_count)
 
-        new_branch = ET.SubElement(branches, 'branch')
-        ET.SubElement(new_branch, 'branch_id').text = str(branch.branch_id)
-        ET.SubElement(new_branch, 'address').text = branch.address
+                self.indent(self.data.getroot())
+                self.data.write(self.file_path)
+                print(f"Данные успешно обновлены")
+                return
 
-        self.indent(self.data.getroot())
-        self.data.write(self.file_path)
-
-        print("Филиал успешно добавлен.")
-
-    def get_branches(self):
-        branches = []
-        for branch_elem in self.root.findall('branches/branch'):
-            branch = Branch(
-                int(branch_elem.find('branch_id').text),
-                branch_elem.find('address').text
-            )
-            branches.append(branch)
-        return branches
-
-    def add_purchase(self, purchase):
-        if (PurchaseServise.check_purchase(quantity=purchase.quantity, product_id=purchase.product_id)):
-            purchases = self.root.find('purchases')
-
-            new_purchase = ET.SubElement(purchases, 'purchase')
-            ET.SubElement(new_purchase, 'purchase_id').text = str(purchase.purchase_id)
-            ET.SubElement(new_purchase, 'date').text = purchase.date
-            ET.SubElement(new_purchase, 'quantity').text = str(purchase.quantity)
-            ET.SubElement(new_purchase, 'client_id').text = str(purchase.client_id)
-            ET.SubElement(new_purchase, 'product_id').text = str(purchase.product_id)
-            ET.SubElement(new_purchase, 'branch_id').text = str(purchase.branch_id)
-
-            self.indent(self.data.getroot())
-            self.data.write(self.file_path)
-
-            print("Сделка успешно добавлена.")
-        else:
-            print("Ошибка ввода данных")
-
-    def get_purchases(self):
-        purchases = []
-        for purchase_elem in self.root.findall('purchases/purchase'):
-            purchase = Purchase(
-                int(purchase_elem.find('purchase_id').text),
-                purchase_elem.find('date').text,
-                int(purchase_elem.find('quantity').text),
-                int(purchase_elem.find('client_id').text),
-                int(purchase_elem.find('product_id').text),
-                int(purchase_elem.find('branch_id').text),
-            )
-            purchases.append(purchase)
-        return purchases
-
-    def add_provider(self, provider):
-        providers = self.root.find('providers')
-
-        new_provider = ET.SubElement(providers, 'provider')
-        ET.SubElement(new_provider, 'provider_id').text = str(provider.provider_id)
-        ET.SubElement(new_provider, 'name').text = provider.name
-
-        self.indent(self.data.getroot())
-        self.data.write(self.file_path)
-
-        print("Филиал успешно добавлен.")
-
-    def get_providers(self):
-        providers = []
-        for provider_elem in self.root.findall('providers/provider'):
-            provider = Provider(
-                int(provider_elem.find('provider_id').text),
-                provider_elem.find('name').text
-            )
-            providers.append(provider)
-        return providers
 
     def indent(self, elem, level=0):
         indent = "\n" + level * "    "
@@ -312,50 +259,21 @@ class XMLRepository:
 # Инициализация репозитория
 repository = XMLRepository('data.xml')
 
-# Пример добавления клиента и чтения из репозитория
-client = Client(client_id=1, full_name="Иванов Иван", address="ул. Пушкина, д.1", phone="123-456-789")
-repository.add_client(client)
 
-# Чтение всех клиентов из репозитория
-clients = repository.get_clients()
-for client in clients:
-    print(client)
+# branch = Branch(address="Lepino, 25")
+# ServiceLayer.add_object(branch)
 
-branch = Branch(branch_id=1, address="ул. Ленина, 5")
-repository.add_branch(branch)
+# client = Client(full_name="Ivanov Ivan", address="Mira, 28", phone="123-456-789")
+# ServiceLayer.add_object(client)
 
-branches = repository.get_branches()
-for branch in branches:
-    print(branch)
+# product = Product(name="Shampoo", price=200, count=0)
+# ServiceLayer.add_object(product)
 
-administrator = Administrator(administrator_id=1, full_name="Иванов Иван", phone="123-456-789", branch_id=1, time_job="9-16")
-repository.add_administrator(administrator)
 
-administrators = repository.get_administrators()
-for administrator in administrators:
-    print(administrator)
+# provider = Provider(name="OOO Petrov ")
+# ServiceLayer.add_object(provider)
 
-product = Product(product_id=1, name="Шампунь", price=200, count=100)
-repository.add_product(product)
+purchase = Purchase(date="12-07-2024", quantity=5, client_id=1, product_id=1, branch_id=1)
+ServiceLayer.add_object(purchase)
 
-products = repository.get_products()
-for product in products:
-    print(product)
-
-purchase = Purchase(purchase_id=1, date="12-07-2024", quantity=50, client_id=100, product_id=1, branch_id=1)
-repository.add_purchase(purchase)
-
-purchases = repository.get_purchases()
-for purchase in purchases:
-    print(purchase)
-
-provider = Provider(provider_id=1, name="ООО Петров ")
-repository.add_provider(provider)
-
-providers = repository.get_providers()
-for provider in providers:
-    print(provider)
-
-repository.add_product_count(1, 100, 1)
-for product in products:
-    print(product)
+# ServiceLayer.order_a_product(1, 1, 20)
